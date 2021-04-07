5173e18cc0d8d7ca16991274d4bda6358285c291
Alam Sanchez <asancheztorres1905@gmail.com>
2021-04-07 00:48:39 -0500

    Agregar interacción con filtros

--- src/components/Marcador.js
+++ src/components/Marcador.js
@@ -1,4 +1,11 @@
-import React, { Component } from 'react';
+import React, {
+    Component,
+    useCallback,
+    useEffect,
+    useMemo,
+    useRef,
+    useState,
+} from 'react';
 import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 import { divIcon } from 'leaflet';
@@ -7,127 +14,135 @@ import 'leaflet/dist/leaflet.css';
 import { Marker, Popup } from 'react-leaflet';
 import { Button } from 'react-bootstrap';
 
+const renderMarker = (label, status) => {
+    return (
+        <div
+            className={`marker-${status}`}
+            style={{
+                fontSize: '12px',
+                width: '36px',
+                height: '36px',
+                borderRadius: '50%',
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+            }}
+        >
+            {label}
+        </div>
+    );
+};
+
 /**
  * Marcador
  * @augments {Component<Props, State>}
  */
-class Marcador extends Component {
-    constructor(props) {
-        super(props);
-        this.marker = document.createElement('div');
-        this.updateMarker(this.props.label, this.props.status);
-    }
+function Marcador({
+    label,
+    status,
+    position,
+    current,
+    provider,
+    locationStr,
+    labels,
+    lastUpdate,
+    map,
+    ...props
+}) {
+    const marker = useMemo(() => document.createElement('div'), []);
+    const popupRef = useRef();
+    const [icon, setIcon] = useState(null);
 
-    updateMarker(label, status) {
-        ReactDOM.render(this.renderMarker(label, status), this.marker);
+    const updateMarker = useCallback(
+        (label_, status_) => {
+            ReactDOM.render(renderMarker(label_, status_), marker);
 
-        this.icon = divIcon({
-            html: this.marker,
-            className: 'sensor-icon',
-            popupAnchor: [7, 0],
-        });
-    }
+            setIcon(
+                divIcon({
+                    html: marker,
+                    className: 'sensor-icon',
+                    popupAnchor: [7, 0],
+                })
+            );
+        },
+        [marker]
+    );
 
-    componentDidUpdate() {
-        this.updateMarker(this.props.label, this.props.status);
-    }
+    useEffect(() => {
+        updateMarker(label, status);
+    }, [label, status, updateMarker]);
 
-    renderMarker(label, status) {
-        return (
-            <div
-                className={`marker-${status}`}
-                style={{
-                    fontSize: '12px',
-                    width: '36px',
-                    height: '36px',
-                    borderRadius: '50%',
-                    display: 'flex',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                }}
+    if (!icon) return null;
+
+    return (
+        <Marker position={position} icon={icon} {...props}>
+            <Popup
+                ref={popupRef}
+                className="custom-popup"
+                closeButton={(props) => (
+                    <button
+                        {...props}
+                        type="button"
+                        class="btn-close"
+                        aria-label="Close"
+                    ></button>
+                )}
             >
-                {label}
-            </div>
-        );
-    }
-
-    render() {
-        const { position, current, provider, locationStr, labels } = this.props;
-
-        return (
-            <Marker position={position} icon={this.icon}>
-                <Popup
-                    className="custom-popup"
-                    closeButton={(props) => (
-                        <button
-                            {...props}
-                            type="button"
-                            class="btn-close"
-                            aria-label="Close"
-                        ></button>
-                    )}
-                >
-                    <div className="px-3 py-2">
-                        <div
-                            className={`rounded marker-${current.status}`}
-                            style={{
-                                display: 'flex',
-                                justifyContent: 'center',
-                                padding: '0 0.1rem ',
-                            }}
-                        >
-                            {current.indicator}: {current.label} {current.units}
-                        </div>
-                        <div className="data-label">
-                            <small className="text-muted">Ubicación</small>
-                            <br />
-                            <data>{locationStr}</data>
-                        </div>
-                        <div className="data-label">
-                            <small className="text-muted">
-                                Última actualización
-                            </small>
-                            <br />
-                            <time>
-                                {new Date(
-                                    this.props.lastUpdate
-                                ).toLocaleString()}
-                            </time>
-                        </div>
-                        <div className="data-label">
-                            <small className="text-muted">Tiempo Real</small>
-                        </div>
-                        <div>
-                            {labels.map(
-                                ({ label, status, value, units, ref }, idx) => (
+                <div className="px-3 py-2">
+                    <div
+                        className={`rounded marker-${current.status}`}
+                        style={{
+                            display: 'flex',
+                            justifyContent: 'center',
+                            padding: '0 0.1rem ',
+                        }}
+                    >
+                        {current.indicator}: {current.label} {current.units}
+                    </div>
+                    <div className="data-label">
+                        <small className="text-muted">Ubicación</small>
+                        <br />
+                        <data>{locationStr}</data>
+                    </div>
+                    <div className="data-label">
+                        <small className="text-muted">
+                            Última actualización
+                        </small>
+                        <br />
+                        <time>{new Date(lastUpdate).toLocaleString()}</time>
+                    </div>
+                    <div className="data-label">
+                        <small className="text-muted">Tiempo Real</small>
+                    </div>
+                    <div>
+                        {labels.map(
+                            ({ label, status, value, units, ref }, idx) => (
+                                <div
+                                    key={idx}
+                                    className="d-flex justify-content-between rt-data-row mb-1"
+                                >
+                                    <a href={ref}>{label}</a>
                                     <div
-                                        key={idx}
-                                        className="d-flex justify-content-between rt-data-row mb-1"
+                                        className={`d-inline-flex justify-content-between px-1 rounded marker-${status}`}
                                     >
-                                        <a href={ref}>{label}</a>
-                                        <div
-                                            className={`d-inline-flex justify-content-between px-1 rounded marker-${status}`}
-                                        >
-                                            <span>{value}</span>
-                                            <span>{units}</span>
-                                        </div>
+                                        <span>{value}</span>
+                                        <span>{units}</span>
                                     </div>
-                                )
-                            )}
-                        </div>
+                                </div>
+                            )
+                        )}
                     </div>
+                </div>
 
-                    <div className="py-2 px-3 border-top text-center">
-                        <Button size="sm">Más información</Button>
-                        <p className="lh-sm mt-2 mb-0">
-                            Fuente(s):{' '}
-                            <a href={provider.ref}>{provider.name}</a>
-                        </p>
-                    </div>
-                </Popup>
-            </Marker>
-        );
-    }
+                <div className="py-2 px-3 border-top text-center">
+                    <Button size="sm">Más información</Button>
+                    <p className="lh-sm mt-2 mb-0">
+                        Fuente(s): <a href={provider.ref}>{provider.name}</a>
+                    </p>
+                </div>
+            </Popup>
+        </Marker>
+    );
 }
 
 Marcador.defaultProps = {};
