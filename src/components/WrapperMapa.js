c0f22e3c1445fa2194ddf62196b81955236a21e1
Alam Sanchez <asancheztorres1905@gmail.com>
2021-04-29 13:24:34 -0500

    Cambiar tiles de mapa

--- src/components/WrapperMapa.js
+++ src/components/WrapperMapa.js
@@ -21,9 +21,8 @@ function Wrapper({ center, children, ...props }) {
             {...props}
         >
             <TileLayer
-                attribution='&copy; <a href="http://osm.org/copyright
-                    ">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+                url="https://tiles.stadiamaps.com/tiles/outdoors/{z}/{x}/{y}{r}.png"
             />
             {children}
         </MapContainer>
