d5b8ef1573b1ee54e5f4c5cf80a35a3133507a8b
Alam Sanchez <asancheztorres1905@gmail.com>
2021-04-29 19:47:13 -0500

    merge

--- src/Registro.js
+++ src/Registro.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import RHFiltros from './components/RHFiltros';
 import Grafica from './components/Grafica';
 import Calendario from './components/Calendario';
-import Heatmap from './components/Heatmap';
+import Heatmap from './components/HeatMap';
 import moment from 'moment';
 import 'moment/locale/es';
 import { apiUrl } from './constants';
