8a042e07d30559e399a971e452c1ee59c8192c1c
joelreyna1104 <joelreyna1104@gmail.com>
2021-07-09 23:41:16 -0500

    Agregar leyenda Proyecto Solidario TEC en el footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,12 +6,13 @@ const Footer = () => {
             <div className="container">
                 <div className="row">
                     <div className="col-auto py-3">
-                        <p className="mb-1">
+                        <p className="mb-1 mt-2">
                             Los datos oficiales provienen de{' '}
                             <a className='text-white' href="https://sinaica.inecc.gob.mx/">Sinaica</a>{' '}
                             y de <a className='text-white' href="http://aire.nl.gob.mx/">Sima</a>
                         </p>
-                        <p>Patrocinado por Natuuramika® 2021</p>
+                        <p className="mb-1">Patrocinado por Natuuramika® 2021</p>
+                        <p>Proyecto Solidario TEC de Monterrey</p>
                     </div>
                 </div>
             </div>
