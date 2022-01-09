8b973850210b1e94fd0867c421b5927f7bb29053
DiegoJRR <diegojrr00@gmail.com>
2022-01-09 12:22:43 -0600

    revert

--- src/App.js
+++ src/App.js
@@ -4,22 +4,20 @@ import { Route, Switch, BrowserRouter } from "react-router-dom";
 import Header from "./components/Header.js";
 import Footer from "./components/Footer.js";
 
-const Mapa = lazy(() => import("./Mapa"));
-const Registro = lazy(() => import("./Registro"));
-const Compara = lazy(() => import("./Compara"));
+import Mapa from "./Mapa";
+import Registro from "./Registro";
+import Compara from "./Compara";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Header />
-        <Suspense fallback={<div>Loading...</div>}>
-          <Switch>
-            <Route exact path="/" component={Mapa} />
-            <Route exact path="/registro" component={Registro} />
-            <Route exact path="/compara" component={Compara} />
-          </Switch>
-        </Suspense>
+        <Switch>
+          <Route exact path="/" component={Mapa} />
+          <Route exact path="/registro" component={Registro} />
+          <Route exact path="/compara" component={Compara} />
+        </Switch>
         <Footer />
       </BrowserRouter>
     </div>
