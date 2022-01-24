77c6860539c321d87e16cae73aea624927a72ed2
Armando <A01570376@itesm.mx>
2022-01-24 16:44:19 -0600

    calendar Heatmap

--- src/components/DatePicker.js
+++ src/components/DatePicker.js
@@ -1,25 +1,142 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react';
+import { apiUrl } from '../constants';
+import { criteria } from '../handlers/statusCriteria';
+import { unidad } from '../constants';
 import buildCalendar from './buildCalendar';
 import { Col, Row } from 'react-bootstrap';
 import { v4 } from 'uuid';
 
+const colors = ['rgb(240, 239, 239)',  //N/D
+                "rgb(76, 187, 23)", 
+                "rgb(253, 218, 13)", 
+                "rgb(255, 172, 28)", 
+                "rgb(238, 75, 43)", 
+                "rgb(143, 63, 151)"];
+
 // Componente para el calendario en la página de Registro Histórico
-const DatePicker = ({ value, onChange }) => {
+const DatePicker = ({ q, fecha, ubic, ind, value, onChange }) => {
     /* 
         Se siguió este tutorial : https://youtu.be/5jRrVqRWqsM
 
         Parámetros:
             - value: día seleccionado
             - onChange: función para cambiar el State de value
+            - fecha : dia seleccionada en el calendario
+            - ubic : sensores seleccionados en los filtros
     */
 
     const [calendar, setCalendar] = useState([]);
 
-    // build calendar
+    const [mes, setMes] = useState(null);
+    const [dataHM, setDataHM] = useState(null);
 
+    const queryAnt = useRef(null);
+    const mesAnt = useRef(mes);
+
+    // build calendar
+    let queryStr = "";
     useEffect(() => {
         setCalendar(buildCalendar(value));
-    }, [value]);
+        if (fecha) {
+            // Cuando cambia la fecha seleccionada en el calendario, solo cambiamos el State del mes si sí cambió el mes
+            if (!mes || !mes.isSame(fecha.clone().startOf('month'))) {
+                mesAnt.current = mes;
+                setMes(fecha.clone().startOf('month'));
+            }
+        }
+        let newq = "";
+        if (q) {
+            newq = q.substring(0, q.indexOf("&inicio")); // solo nos interesa saber las ubicaciones y gases seleccionados
+        }
+
+        if (!queryAnt.current || queryAnt.current !== newq || mesAnt.current != mes) { // Cambió la ubicación y gases seleccionados o el mes seleccionado
+            if (queryAnt.current !== q) queryAnt.current = newq;
+            if (mesAnt.current != mes) mesAnt.current = mes;
+
+            if (ubic && mes) {
+                // Creamos query, sacando los datos de todo el mes
+                queryStr = 'ubic=';
+                queryStr += ubic.value
+
+                queryStr +=
+                    '&ind=' + ind +
+                    '&inicio=' + mes.clone().format("YYYY-MM-DD") +
+                    '&fin=' + mes.clone().endOf('month').format('YYYY-MM-DD');
+
+
+                fetch(`${apiUrl}/prom-data?${queryStr}`)
+                    .then(response => response.json())
+                    .then((json) => setDataHM(json))
+                    .catch((e) => console.log(e));
+
+            }
+        }
+    }, [value, fecha, q, mes]);
+
+    var series = []; // Datos
+
+    if (q && dataHM && dataHM.length > 0) {
+        /* EJEMPLO DEL FORMATO
+        series:[
+            {
+                name: dataHM[i]["zona"]
+                data: [
+                    {
+                        x: número del día,
+                        y: dataHM[i][ind],
+                        fecha: dataHM[i][fecha].format("DD-MM-YYYY")
+                    }
+                ]
+            }
+        ] */
+        let index = 0;
+        let dataItem = {};
+
+        // Este codigo de abajo se cambio porque ya no era un array de ubicaciones se cambio solo a una ubicación
+
+        // for (let u = 0; u < ubic.length && dataHM[index]; u++) { // Por cada ubicacion seleccionada se crea una fila del heatmap
+        // let currUbic = dataHM[index]["zona"];
+        let currUbic = dataHM[index]["zona"];
+        let primerDia = new Date(dataHM[index].fecha.replace(/-/g, '\/').replace(/T.+/, '')); // primer día registrado de los datos de una ubicacion
+        let currDia = 1;
+
+        let seriesItem = {
+            name: currUbic,
+            data: []
+        };
+
+        // llenar primeros días vacíos si es que los datos del mes no empiezan en el primer día
+        while (primerDia.getDate() !== currDia) {
+            dataItem.x = currDia;
+            dataItem.y = -1;
+            seriesItem.data.push({ ...dataItem });
+            currDia++;
+        }
+
+
+        // ahora sí hay datos
+        while (index < dataHM.length && dataHM[index]["zona"] === currUbic) {
+            dataItem.x = currDia;
+
+            // revisa que sí haya un registro de ese dia checando si coincide el número de día
+            if (currDia === parseInt(dataHM[index]["fecha"].substring(8))) {
+                if (dataHM[index]["prom"] !== "") {
+                    dataItem.y = dataHM[index]["prom"];
+                    dataItem.fecha = dataHM[index]["fecha"];
+                } else {
+                    dataItem.y = -1;
+                }
+                index++;
+            } else {
+                dataItem.y = -1;
+            }
+
+            seriesItem.data.push({ ...dataItem });
+            currDia++;
+        }
+
+        series.push({ ...seriesItem });
+    }
 
     function isSelected(day) {
         return value.isSame(day, "day");
@@ -36,6 +153,50 @@ const DatePicker = ({ value, onChange }) => {
     function sameMonth(day) {
         return day.isSame(value, "month");
     }
+
+    function colorStatus(day) {
+        if(series.length !== 0)
+        {
+            const x = series[0]["data"][day.format('D')-1]
+            if (typeof(x) !== "undefined")
+            {
+                const y = x["y"]
+
+                if (y < 0) {
+                    return colors[0] // No Data
+                } 
+                if (y < criteria['Aire y Salud'][ind][0]) {
+                    return colors[1]
+                }
+                if (y < criteria['Aire y Salud'][ind][1]) {
+                    return colors[2]
+                }
+                if (y < criteria['Aire y Salud'][ind][2]) {
+                    return colors[3]
+                }
+                if (y < criteria['Aire y Salud'][ind][3]) {
+                    return colors[4]
+                }
+                if (y > criteria['Aire y Salud'][ind][3]) {
+                    return colors[5]
+                }
+            }    
+        }
+       return colors[0]
+    }
+
+    function toolTip(day) {
+        if(series.length !== 0)
+        {
+            const x = series[0]["data"][day.format('D')-1]
+            if (typeof(x) !== "undefined")
+            {
+                const y = x["y"].toFixed(2)
+                return y + ' ' + unidad[ind] 
+            }    
+        }
+       return "N/D" 
+    }    
 
     function dayStyles(day) {
         if (isSelected(day)) return "selected"
@@ -98,8 +259,9 @@ const DatePicker = ({ value, onChange }) => {
                             {
                                 week.map((day, i) => <div className="day"
                                     onClick={() => !afterToday(day) && onChange(day)}>
-                                    <div className={dayStyles(day)} key={v4()}>
+                                    <div className={dayStyles(day)} style={{ backgroundColor: colorStatus(day), "borderRadius": "5px" }}key={v4()}>
                                         {day.format("D")}
+                                        <span>{toolTip(day)}</span>
                                     </div>
                                 </div>
                                 )
