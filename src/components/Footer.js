1a9663f0b9ce602d383b2b7b37237c7480f29679
Silvestre Garcia <a00829450@itesm.mx>
2022-07-12 20:11:16 -0500

    Agregue y acomode filtros de compara datos

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,8 +8,8 @@ const Footer = () => {
                     <div className="col-auto py-3">
                         <p className="mb-1 mt-2">
                             Los datos oficiales provienen de{' '}
-                            <a className='text-white' href="https://sinaica.inecc.gob.mx/">Sinaica</a>{' '}
-                            y de <a className='text-white' href="http://aire.nl.gob.mx/">Sima</a>
+                            <a style={{textDecoration: "underline"}} className='text-white' href="https://sinaica.inecc.gob.mx/">Sinaica</a>{' '}
+                            y de <a style={{textDecoration: "underline"}} className='text-white' href="http://aire.nl.gob.mx/">Sima</a>
                         </p>
                         {/* <p className="mb-1">Patrocinado por Natuuramika® 2021</p> */}
                         <p>Proyecto Solidario con el servicio social del TEC de Monterrey</p>
