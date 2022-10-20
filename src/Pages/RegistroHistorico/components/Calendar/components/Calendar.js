09e0b1275de4eeec1b913bdeadc3af5cd0d9099a
Cristian Urbina <A01412249@tec.mx>
2022-10-20 02:46:41 -0500

    Add react-calendar dependency WIP

--- src/Pages/RegistroHistorico/components/Calendar/components/Calendar.js
+++ src/Pages/RegistroHistorico/components/Calendar/components/Calendar.js
@@ -1,51 +1,82 @@
-import React, { useState, useEffect } from 'react'
-import { Button, Col, Row, Accordion, Card } from 'react-bootstrap'
-import { apiUrl } from "../../../../../constants";
-import DatePicker from './DatePicker'
+import React, { useState, useEffect, useCallback } from "react";
+import { Button, Col, Row, Accordion, Card } from "react-bootstrap";
+import {
+  populateDateRange,
+  getFirstDayOfMonth,
+  getLastDayOfMonth,
+  isSameDay,
+} from "../../../../../utils/PopulateDateRange";
+import Calendar from "react-calendar";
+import 'react-calendar/dist/Calendar.css';
+import moment from "moment";
 import { AiFillCaretDown, AiFillRightSquare } from "react-icons/ai";
-import { getStatus } from '../../../../../handlers/statusCriteria';
-import Select from 'react-select';
-import moment from 'moment'
-import './Calendar.css';
-import 'moment/locale/es';
+import { criteria, getStatus } from "../../../../../handlers/statusCriteria";
+import "./Calendar.css";
+import "./DayBullet.css";
+
+const currentMonth = new Date().getMonth();
+const currentYear = new Date().getUTCFullYear();
+
+const beginOfMonth = getFirstDayOfMonth(currentYear, currentMonth);
+const endOfMonth = getLastDayOfMonth(currentYear, currentMonth);
+
+const dateFormat = { weekday: "long", month: "short", day: "numeric", year: "numeric" };
+
+const DayBullet = (props) => {
+  const { count, text, type } = props;
+  return (
+    <div className="day-bullet">
+      <div className={`count ${type}`}>{count}</div>
+      <div className="text">{text}</div>
+    </div>
+  );
+};
 
 const unidad = {
-    PM25: "µg/m3",
-    PM10: "µg/m3",
-    CO: "ppm",
-    O3: "ppm",
-    NO2: "ppm",
-    SO2: "ppm"
-}
+  PM25: "µg/m3",
+  PM10: "µg/m3",
+  CO: "ppm",
+  O3: "ppm",
+  NO2: "ppm",
+  SO2: "ppm",
+};
 
 // Valores para los dropdowns:
 const meses = [
-    { value: '1', label: 'enero' },
-    { value: '2', label: 'febrero' },
-    { value: '3', label: 'marzo' },
-    { value: '4', label: 'abril' },
-    { value: '5', label: 'mayo' },
-    { value: '6', label: 'junio' },
-    { value: '7', label: 'julio' },
-    { value: '8', label: 'agosto' },
-    { value: '9', label: 'septiembre' },
-    { value: '10', label: 'octubre' },
-    { value: '11', label: 'noviembre' },
-    { value: '12', label: 'diciembre' }
-]
+  { value: "1", label: "enero" },
+  { value: "2", label: "febrero" },
+  { value: "3", label: "marzo" },
+  { value: "4", label: "abril" },
+  { value: "5", label: "mayo" },
+  { value: "6", label: "junio" },
+  { value: "7", label: "julio" },
+  { value: "8", label: "agosto" },
+  { value: "9", label: "septiembre" },
+  { value: "10", label: "octubre" },
+  { value: "11", label: "noviembre" },
+  { value: "12", label: "diciembre" },
+];
 
 const anios = [
-    { value: '2018', label: '2018' },
-    { value: '2019', label: '2019' },
-    { value: '2020', label: '2020' },
-    { value: '2021', label: '2021' },
-    { value: '2022', label: '2022' },
-
-]
+  { value: "2018", label: "2018" },
+  { value: "2019", label: "2019" },
+  { value: "2020", label: "2020" },
+  { value: "2021", label: "2021" },
+  { value: "2022", label: "2022" },
+];
 
 // Componente para la página de Registro Histórico
-function Calendario({location, data, gas, dataHM, downloadFile, fetchData}) {
-    /* 
+function Calendario({
+  location,
+  data,
+  gas,
+  selectedDate,
+  setSelectedDate,
+  dataHM,
+  downloadFile,
+  fetchData,
+}) {
+  /* 
         Parámetros:
             - q : query creado en los filtros
             - create: función para crear query
@@ -56,166 +87,203 @@ function Calendario({location, data, gas, dataHM, downloadFile, fetchData}) {
             - downloadFile: función para descargar los datos mostrados
     */
 
-    // Función para obtener la clase a la que pertenece según la medida
-    // Dicha clase es para darle el color según el nivel de riesgo 
-    function colorIndice(medida) {
-        let val = getStatus(gas, medida);
+  // Función para obtener la clase a la que pertenece según la medida
+  // Dicha clase es para darle el color según el nivel de riesgo
+  function colorIndice(medida) {
+    let val = getStatus(gas, medida);
 
-        switch (val) {
-            case 0:
-                return "bueno";
-            case 1:
-                return "acept";
-            case 2:
-                return "mala";
-            case 3:
-                return "muymala";
-            case 4:
-                return "extmala";
-            default:
-                return "no-data";
-        }
+    switch (val) {
+      case 0:
+        return "bueno";
+      case 1:
+        return "acept";
+      case 2:
+        return "mala";
+      case 3:
+        return "muymala";
+      case 4:
+        return "extmala";
+      default:
+        return "no-data";
     }
+  }
 
-// Datos calendario
-  const [month, setMonth] = useState(new Date().getMonth() + 1);
-  const [year, setYear] = useState(new Date().getUTCFullYear());
-  const [selectedDate, setSelectedDate] = useState(moment()); // Valor del día que está seleccionado en el calendario, se inicializa con el dia actual
-
-  useEffect(() => {
-    let newSelectedDate = moment();
-    newSelectedDate.month(month-1);
-    newSelectedDate.year(year);
-    setSelectedDate(newSelectedDate);
-  }, [month, year]); // Cada que cambia un valor de los dropdowns, cambiamos el valor del día seleccionado
+  // Datos calendario
+  const [value, onChange] = useState(new Date());
+  const [datesOfTheMonth, setMonthDates] = useState(populateDateRange(beginOfMonth, endOfMonth));
 
   useEffect(() => {
-    if (location && gas)fetchData();
-  }, [selectedDate]);
+    setSelectedDate(moment(value));
+  }, [value]);
+
+  const getDayStatus = useCallback(
+    (date) => {
+      if (dataHM && date.getDate() < dataHM.length) {
+        const dayAverage = dataHM[date.getDate()].prom;
+
+        if (dayAverage < 0) {
+          return "no-data";
+        }
+        if (dayAverage < criteria["Aire y Salud"][gas][0]) {
+          return "good";
+        }
+        if (dayAverage < criteria["Aire y Salud"][gas][1]) {
+          return "acceptable";
+        }
+        if (dayAverage < criteria["Aire y Salud"][gas][2]) {
+          return "bad";
+        }
+        if (dayAverage < criteria["Aire y Salud"][gas][3]) {
+          return "super-bad";
+        }
+        if (dayAverage > criteria["Aire y Salud"][gas][3]) {
+          return "extremly-bad";
+        }
+      }
+      return "no-data";
+    },
+    [dataHM]
+  );
+
+  useEffect(() => {
+    console.log("dataHM changed");
+  }, [dataHM]);
 
   // Cuando ya se hizo el fetch para traer datos:
-    let dataCol1 = [];
-    let dataCol2 = [];
-    let index = 0;
-    let currHour = 0;
+  let dataCol1 = [];
+  let dataCol2 = [];
+  let index = 0;
+  let currHour = 0;
 
+  if (data && data.length !== 0) {
+    // Primera columna (horas de 00:00 a 11:00)
+    while (currHour < 12 && index < data.length) {
+      let ch = "T" + currHour.toString().padStart(2, 0) + ":";
+      let info = [];
 
-    if (data && data.length !== 0) {
-        // Primera columna (horas de 00:00 a 11:00)
-        while (currHour < 12 && index < data.length) {
-            let ch = "T" + currHour.toString().padStart(2, 0) + ":";
-            let info = [];
+      let firstIndex = index;
 
-            let firstIndex = index;
+      // Junta los datos de la misma hora aunque sean de diferente sensor para ponerlos en un mismo acordión
+      while (index < data.length && data[index]["dia"].includes(ch)) {
+        // Se crea un p tag con un bullet de color, ubicación, medida registrada y unidad
+        info.push(
+          <p key={index}>
+            <AiFillRightSquare className={colorIndice(data[index][gas])} />
+            {data[index]["zona"]}:{" "}
+            {data[index][gas] ? ` ${data[index][gas]} ${unidad[gas]}` : " No hay registro"}
+          </p>
+        );
+        index++;
+      }
 
-            // Junta los datos de la misma hora aunque sean de diferente sensor para ponerlos en un mismo acordión
-            while (index < data.length && data[index]["dia"].includes(ch)) {
-                // Se crea un p tag con un bullet de color, ubicación, medida registrada y unidad
-                info.push(<p><AiFillRightSquare className={colorIndice(data[index][gas])} />{data[index]["zona"]}: {data[index][gas] ? ` ${data[index][gas]} ${unidad[gas]}` : " No hay registro"}</p>);
-                index++;
-            }
+      dataCol1.push(
+        <Accordion key={data[firstIndex].Registros_id} id={data[firstIndex].Registros_id}>
+          <Card>
+            <Card.Header>
+              <Accordion.Toggle
+                as={Button}
+                className="hora"
+                variant="link"
+                eventKey={data[firstIndex].Registros_id}
+              >
+                <AiFillCaretDown color="lightgray" /> {currHour.toString().padStart(2, 0)}:00
+              </Accordion.Toggle>
+            </Card.Header>
+            <Accordion.Collapse eventKey={data[firstIndex].Registros_id}>
+              <Card.Body>{info}</Card.Body>
+            </Accordion.Collapse>
+          </Card>
+        </Accordion>
+      );
 
-            dataCol1.push(
-                <Accordion key={data[firstIndex].registros_id} id={data[firstIndex].registros_id}>
-                    <Card>
-                        <Card.Header>
-                            <Accordion.Toggle as={Button} className="hora"
-                                variant="link"
-                                eventKey={data[firstIndex].registros_id}>
-                                <AiFillCaretDown color="lightgray" /> {currHour.toString().padStart(2, 0)}:00
-                            </Accordion.Toggle>
-                        </Card.Header>
-                        <Accordion.Collapse eventKey={data[firstIndex].registros_id}>
-                            <Card.Body>
-                                {info}
-                            </Card.Body>
-                        </Accordion.Collapse>
-                    </Card>
-                </Accordion>
-            )
-
-            currHour++;
-        }
-
-        // Segunda columna (horas de 12:00 a 23:00)
-        while (currHour < 24 && index < data.length) {
-            let ch = "T" + currHour.toString().padStart(2, 0);
-            let info = [];
-
-            let firstIndex = index; // Esta variable sirve para darle un key a cada acordión, sin pasarnos del último índice
-
-            while (index < data.length && data[index]["dia"].includes(ch)) {
-                info.push(<p><AiFillRightSquare className={colorIndice(data[index][gas])} /> {data[index]["zona"]}: {data[index][gas] ? `${data[index][gas]} ${unidad[gas]}` : "No hay registro"}</p>);
-                index++;
-            }
-
-            dataCol2.push(
-                <Accordion key={data[firstIndex].registros_id} id={data[firstIndex].registros_id}>
-                    <Card>
-                        <Card.Header>
-                            <Accordion.Toggle as={Button} className="hora"
-                                variant="link"
-                                eventKey={data[firstIndex].registros_id}>
-                                <AiFillCaretDown color="lightgray" /> {currHour.toString().padStart(2, 0)}:00
-                            </Accordion.Toggle>
-                        </Card.Header>
-                        <Accordion.Collapse eventKey={data[firstIndex].registros_id}>
-                            <Card.Body>
-                                {info}
-                            </Card.Body>
-                        </Accordion.Collapse>
-                    </Card>
-                </Accordion>
-            )
-
-            currHour++;
-        }
+      currHour++;
     }
 
-    return (
-        <div className="container mb-10">
+    // Segunda columna (horas de 12:00 a 23:00)
+    while (currHour < 24 && index < data.length) {
+      let ch = "T" + currHour.toString().padStart(2, 0);
+      let info = [];
 
-            <Row className="mb-5">
-                <Col sm={12} lg={6}>
-                    <div>
-                        <Button className="btn btn-light mb-4 mt-4" block onClick={downloadFile}>Descargar datos del mes</Button>
-                        <Row className="mb-4">
-                            <Col sm={6}>
-                                <Select
-                                    options={meses}
-                                    placeholder={'Mes'}
-                                    onChange={(target) => setMonth(target.value)}
-                                    defaultValue={meses[new Date().getMonth()]}
-                                />
-                            </Col>
-                            <Col sm={6}>
-                                <Select
-                                    options={anios}
-                                    placeholder={'Año'}
-                                    onChange={(target) => setYear(target.value)}
-                                    defaultValue={anios[anios.length - 1]}
-                                />
-                            </Col>
-                        </Row>
-                        <div className="detalles">
-                            <h4>{selectedDate.format("DD/MM/YYYY")} Detalle por hora</h4>
-                            { data ? ( <Row>
-                                <Col sm={6}>
-                                    {dataCol1}
-                                </Col>
-                                <Col sm={6}>
-                                    {dataCol2}
-                                </Col>
-                            </Row> ) : "No hay datos para este día"}
-                        </div>
-                    </div>
-                </Col>
-                <Col sm={12} lg={6} className="d-flex align-items-start justify-content-center">
-                    <DatePicker month={month} year={year} location={location} gas={gas} selectedDate={selectedDate} dataHM={dataHM} onChange={setSelectedDate} />
-                </Col>
-            </Row>
-        </div>
-    )
+      let firstIndex = index; // Esta variable sirve para darle un key a cada acordión, sin pasarnos del último índice
+
+      while (index < data.length && data[index]["dia"].includes(ch)) {
+        info.push(
+          <p key={index}>
+            <AiFillRightSquare className={colorIndice(data[index][gas])} /> {data[index]["zona"]}:{" "}
+            {data[index][gas] ? `${data[index][gas]} ${unidad[gas]}` : "No hay registro"}
+          </p>
+        );
+        index++;
+      }
+
+      dataCol2.push(
+        <Accordion key={data[firstIndex].Registros_id} id={data[firstIndex].Registros_id}>
+          <Card>
+            <Card.Header>
+              <Accordion.Toggle
+                as={Button}
+                className="hora"
+                variant="link"
+                eventKey={data[firstIndex].Registros_id}
+              >
+                <AiFillCaretDown color="lightgray" /> {currHour.toString().padStart(2, 0)}:00
+              </Accordion.Toggle>
+            </Card.Header>
+            <Accordion.Collapse eventKey={data[firstIndex].Registros_id}>
+              <Card.Body>{info}</Card.Body>
+            </Accordion.Collapse>
+          </Card>
+        </Accordion>
+      );
+
+      currHour++;
+    }
+  }
+
+  const tileClassName = useCallback(
+    ({ date, view }) => {
+      // Check if a date React-Calendar wants to check is on the list of dates to add class to
+      if (
+        datesOfTheMonth.find((dDate) => {
+          return isSameDay(dDate, date);
+        })
+      ) {
+        return getDayStatus(date);
+      }
+    },
+    [datesOfTheMonth, getDayStatus]
+  );
+
+  return (
+    <div className="container mb-10">
+      <Row className="mb-5">
+        <Col sm={12} lg={6}>
+          <div>
+            <Button className="btn btn-light mb-4 mt-4" block onClick={downloadFile}>
+              Descargar datos del mes
+            </Button>
+            <div className="detalles">
+              <p>
+                Detalle por hora del día{" "}
+                <span className="current-day">{value.toLocaleDateString("es-MX", dateFormat)}</span>
+              </p>
+              {data ? (
+                <Row>
+                  <Col sm={6}>{dataCol1}</Col>
+                  <Col sm={6}>{dataCol2}</Col>
+                </Row>
+              ) : (
+                "No hay datos para este día"
+              )}
+            </div>
+          </div>
+        </Col>
+        <Col sm={12} lg={6} className="d-flex align-items-start justify-content-center">
+          <Calendar onChange={onChange} value={value} tileClassName={tileClassName} />
+        </Col>
+      </Row>
+    </div>
+  );
 }
 
-export default Calendario
+export default Calendario;
