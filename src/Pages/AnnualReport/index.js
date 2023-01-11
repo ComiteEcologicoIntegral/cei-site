997301799ea2bcf184b4ec6506f7eb61e6d3c98f
Cristian Urbina <urbina1509@gmail.com>
2023-01-10 21:06:51 -0600

    Fix multiple table renders and add years

--- src/Pages/AnnualReport/index.js
+++ src/Pages/AnnualReport/index.js
@@ -1,15 +1,16 @@
-import React, {useState, useEffect } from 'react'
-import {Form, Col} from "react-bootstrap";
+import React, {useState, useEffect} from 'react'
+import {Form, Col, Button} from "react-bootstrap";
 import Select from "react-select";
 import {getSensorLocationsBySystem} from '../../handlers/data';
-import { populateDateRange, getFirstDayOfMonth, getLastDayOfMonth } from '../../utils/PopulateDateRange';
-import { gasesOptions, apiUrl, normOptions } from "../../constants";
-import { getDateStatusClassName } from "../../handlers/statusCriteria";
+import {populateDateRange, getFirstDayOfMonth, getLastDayOfMonth} from '../../utils/PopulateDateRange';
+import {gasesOptions, apiUrl, normOptions} from "../../constants";
+import {getDateStatusClassName} from "../../handlers/statusCriteria";
 import "./index.css";
 
 export default function AnnualReport() {
   const REPORT_SYSTEM = {value: "AireNuevoLeon", label: "AireNuevoLeon/Sinaica", opt: "G"};
-const months = [
+
+  const months = [
     {value: 0, label: "Enero"},
     {value: 1, label: "Febrero"},
     {value: 2, label: "Marzo"},
@@ -25,43 +26,44 @@ const months = [
   ];
 
   const years = [
+    {value: 2019, label: 2019},
+    {value: 2020, label: 2020},
+    {value: 2021, label: 2021},
     {value: 2022, label: 2022},
   ];
 
+  // dropdowns
   const [locations, setLocations] = useState([]);
   const [avgType, setAvgType] = useState([]);
   const [month, setMonth] = useState(months[0]);
   const [year, setYear] = useState(years[0]);
   const [monthDates, setMonthDates] = useState([]);
   const [contaminant, setContaminant] = useState(gasesOptions[0]);
-  const [monthData, setMonthData] = useState({});
+  const [monthData, setMonthData] = useState(null);
   const [avgOptions, setAvgOptions] = useState(null);
 
+  // table 
+  const [avgTypeTable, setAvgTypeTable] = useState(null);
+  const [contaminantTable, setContaminantTable] = useState(null);
+  const [monthTable, setMonthTable] = useState(null);
+  const [yearTable, setYearTable] = useState(null);
+
+  useEffect(() => {
+    getSensorLocationsBySystem(REPORT_SYSTEM).then((data) => {
+      setLocations(data);
+    });
+  }, [])
+
+  // Make the avgType contsistent with the current selected contaminant
   useEffect(() => {
     if (!contaminant) return;
     setAvgType(normOptions[contaminant.value][0]);
     setAvgOptions(normOptions[contaminant.value]);
   }, [contaminant]);
 
-  useEffect(() => {
-    getSensorLocationsBySystem(REPORT_SYSTEM).then((data) => {
-      setLocations(data);
-    }); 
-  }, [])
-
-  useEffect(() => {
-    const currYear = year.value;
-    const currMonth = month.value;
-    const firstDayOfTheMonth = getFirstDayOfMonth(currYear, currMonth);
-    const lastDayOfTheMonth = getLastDayOfMonth(currYear, currMonth);
-    const tmpMonthDates = populateDateRange(firstDayOfTheMonth, lastDayOfTheMonth);
-    setMonthDates(tmpMonthDates);
-  }, [month, year])
-
   function getCalendarQueryString(location) {
-    
-    let start = monthDates[0];
-    let end = monthDates[monthDates.length - 1];
+    let start = getFirstDayOfMonth(year.value, month.value);
+    let end = getLastDayOfMonth(year.value, month.value);
 
     let startDateString = start.toISOString().split("T")[0];
     let endDateString = end.toISOString().split("T")[0];
@@ -84,39 +86,51 @@ const months = [
   const fetchCalendarData = (location) => {
     // Data for calendar
     let calendarQueryString = getCalendarQueryString(location);
-    return new Promise((resolve, reject) =>  {
-    fetch(`${apiUrl}/prom-data-norms?${calendarQueryString}`)
-      .then((response) => response.json())
-      .then((json) => {
-        resolve(json);
-      })
-      .catch((e) => reject(e));
+    return new Promise((resolve, reject) => {
+      fetch(`${apiUrl}/prom-data-norms?${calendarQueryString}`)
+        .then((response) => response.json())
+        .then((json) => {
+          resolve(json);
+        })
+        .catch((e) => reject(e));
     }
     );
   };
 
-  useEffect( () => {
+  function getDateStatus(date, location) {
+    if (!monthData[location.label] || !date || date.getDate() > monthData[location.label].length) return;
+    const dayAverage = monthData[location.label][date.getDate() - 1].movil;
+    if (dayAverage === "") return;
+    let ans = getDateStatusClassName(dayAverage, contaminantTable.value, avgTypeTable.value);
+    return ans;
+  }
+
+  const handleClick = () => {
+
+    setMonthData(null);
+
+    const firstDayOfTheMonth = getFirstDayOfMonth(year.value, month.value);
+    const lastDayOfTheMonth = getLastDayOfMonth(year.value, month.value);
+    const tmpMonthDates = populateDateRange(firstDayOfTheMonth, lastDayOfTheMonth);
+    setMonthDates(tmpMonthDates);
+
+    setAvgTypeTable(avgType);
+    setContaminantTable(contaminant);
+    setMonthTable(month);
+    setYearTable(year);
+
     const promises = [];
     locations.forEach((location) => {
       promises.push(fetchCalendarData(location));
     });
-    setMonthData({});
 
-    Promise.all(promises).then( (results) => {
+    Promise.all(promises).then((results) => {
       let tmpMonthData = {}
       for (let i = 0; i < results.length; ++i) {
         tmpMonthData[locations[i].label] = results[i];
       }
       setMonthData(tmpMonthData);
     });
-  }, [monthDates, locations, contaminant, avgType]);
-
-  function getDateStatus(date, location) {
-    if (!monthData[location.label] || !date || date.getDate() > monthData[location.label].length) return;
-    const dayAverage = monthData[location.label][date.getDate()-1].movil;
-    if (dayAverage === "") return;
-    let ans = getDateStatusClassName(dayAverage, contaminant.value, avgType.value); 
-    return ans;
   }
 
   return (
@@ -162,25 +176,37 @@ const months = [
             />
           </Col>
         </Form.Row>
+        <Button onClick={handleClick}>
+          Obtener datos
+        </Button>
       </Form>
-      <table>
-        <tr>
-          <th>Zona</th>
-          {monthDates.map((date) => {
-            return (<th key={date.getDate()}>{date.getDate()}</th>);
-          })}
-        </tr>
-        {locations.map((location) => {
-          if (!monthData) return;
-          const monthDataForLocation = monthData[location.label];
-          if (!monthDataForLocation) return;
-          const dayAvgs = monthDataForLocation.map((data, i) => {
-            return(<td className={getDateStatus(monthDates[i], location)}>{data.movil ? data.movil.toPrecision(4) : "ND"}</td>);
-          });
-          return(<tr><td>{location.label}</td>{dayAvgs}</tr>);
-        }
-        )}
-      </table>
+      {monthData &&
+        <div className="annual-report-table">
+          <h4>Tabla para contaminante {contaminantTable.label} con la norma {avgTypeTable.label}. {monthTable.label} {yearTable.label}</h4>
+          <table>
+            <thead>
+              <tr>
+                <th>Zona</th>
+                {monthDates.map((date) => {
+                  return (<th key={date.getDate()}>{date.getDate()}</th>);
+                })}
+              </tr>
+            </thead>
+            <tbody>
+              {locations.map((location) => {
+                if (!monthData) return;
+                const monthDataForLocation = monthData[location.label];
+                if (!monthDataForLocation) return;
+                const dayAvgs = monthDataForLocation.map((data, i) => {
+                  return (<td className={getDateStatus(monthDates[i], location)}>{data.movil ? data.movil.toPrecision(4) : "ND"}</td>);
+                });
+                return (<tr><td>{location.label}</td>{dayAvgs}</tr>);
+              }
+              )}
+            </tbody>
+          </table>
+        </div>
+      }
     </div>
   );
 }
