e57f5ee4cfb7c2f8453674dd5785589f77cba398
SofiRegiM <108156450+SofiRegiM@users.noreply.github.com>
2023-04-09 16:17:18 -0600

    Update statusCriteria.js
    Función getStatusOMS hasta abajo, se va a usar en Marcador.js

--- src/handlers/statusCriteria.js
+++ src/handlers/statusCriteria.js
@@ -70,3 +70,57 @@ export function getStatusClassName(avg, gas, norm) {
       return statusClassName.ExtremelyBad;
     }
   }
+export function getStatusOMS(avg, gas, norm) {
+  if (avg < 0 || !avg || avg === "" || avg ==="ND") {
+    return statusClassName.NoData;
+  }
+  else if (avg > 0 && avg <= 4.9 && gas === "PM25") { 
+    
+    avg = criteria[norm][gas][0]             
+    return statusClassName.Good;
+  }
+  else if (avg >= 5 && gas === "PM25") { 
+    avg = criteria[norm][gas][3]             
+    return statusClassName.SuperBad;
+  }
+  else if (avg > 0 && avg <= 44.5 && gas === "PM10") { 
+    avg = criteria[norm][gas][0]             
+    return statusClassName.Good;
+  }
+  else if (avg >= 45 && gas === "PM10") { 
+    avg = criteria[norm][gas][3]             
+    return statusClassName.SuperBad;
+  }
+  else if (avg > 0 && avg <= 99.9 && gas === "O3") { 
+    avg = criteria[norm][gas][0]             
+    return statusClassName.Good;
+  }
+  else if (avg >= 100 && gas === "O3") { 
+    avg = criteria[norm][gas][3]             
+    return statusClassName.SuperBad;
+  }
+  else if (avg > 0 && avg <= 24.9 && gas === "NO2") { 
+    avg = criteria[norm][gas][0]             
+    return statusClassName.Good;
+  }
+  else if (avg >= 25 && gas === "NO2") { 
+    avg = criteria[norm][gas][3]             
+    return statusClassName.SuperBad;
+  }
+  else if (avg > 0 && avg <= 39.9 && gas === "SO2") { 
+    avg = criteria[norm][gas][0]             
+    return statusClassName.Good;
+  }
+  else if (avg >= 40 && gas === "SO2") { 
+    avg = criteria[norm][gas][3]             
+    return statusClassName.SuperBad;
+  }
+  else if (avg > 0 && avg <= 3999 && gas === "CO") { 
+    avg = criteria[norm][gas][0]             
+    return statusClassName.Good;
+  }
+  else if (avg >=4000 && gas === "CO") { 
+    avg = criteria[norm][gas][3]             
+    return statusClassName.SuperBad;
+  }
+  }
