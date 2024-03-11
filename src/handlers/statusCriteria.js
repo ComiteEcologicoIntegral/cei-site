3d802328b465031f4db85bd20da8b2b23e664399
Diego de Jesús Ramírez Rodríguez <diegojrr00@gmail.com>
2024-03-10 18:18:39 -0700

    remove unused code

--- src/handlers/statusCriteria.js
+++ src/handlers/statusCriteria.js
@@ -62,16 +62,12 @@ export function getStatusClassName(avg, gas, norm) {
   if (avg < 0 || !avg || avg === "" || avg === "ND") {
     return statusClassName.NoData;
   } else if (avg > 0 && avg <= 50) {
-    avg = criteria[norm][gas][0];
     return statusClassName.Good;
   } else if (avg >= 51 && avg <= 100) {
-    avg = criteria[norm][gas][1];
     return statusClassName.Acceptable;
   } else if (avg >= 101 && avg <= 150) {
-    avg = criteria[norm][gas][2];
     return statusClassName.Bad;
   } else if (avg >= 151 && avg <= 200) {
-    avg = criteria[norm][gas][3];
     return statusClassName.SuperBad;
   } else {
     return statusClassName.ExtremelyBad;
@@ -85,7 +81,7 @@ export function getICAR(avg, gas, norm) {
     return statusClassName.NoData;
   }
 
-  for (var limit_index = 0; limit_index < limits.length; limit_index++) {
+  for (let limit_index = 0; limit_index < limits.length; limit_index++) {
     if (avg <= limits[limit_index]) {
       return statusClassName[statusClassOrder[limit_index]];
     }
@@ -98,40 +94,28 @@ export function getStatusOMS(avg, gas, norm) {
   if (avg < 0 || !avg || avg === "" || avg === "ND") {
     return statusClassName.NoData;
   } else if (avg > 0 && avg <= 4.9 && gas === "PM25") {
-    avg = criteria[norm][gas][0];
     return statusClassName.Good;
   } else if (avg >= 5 && gas === "PM25") {
-    avg = criteria[norm][gas][3];
     return statusClassName.SuperBad;
   } else if (avg > 0 && avg <= 44.5 && gas === "PM10") {
-    avg = criteria[norm][gas][0];
     return statusClassName.Good;
   } else if (avg >= 45 && gas === "PM10") {
-    avg = criteria[norm][gas][3];
     return statusClassName.SuperBad;
   } else if (avg > 0 && avg <= 99.9 && gas === "O3") {
-    avg = criteria[norm][gas][0];
     return statusClassName.Good;
   } else if (avg >= 100 && gas === "O3") {
-    avg = criteria[norm][gas][3];
     return statusClassName.SuperBad;
   } else if (avg > 0 && avg <= 24.9 && gas === "NO2") {
-    avg = criteria[norm][gas][0];
     return statusClassName.Good;
   } else if (avg >= 25 && gas === "NO2") {
-    avg = criteria[norm][gas][3];
     return statusClassName.SuperBad;
   } else if (avg > 0 && avg <= 39.9 && gas === "SO2") {
-    avg = criteria[norm][gas][0];
     return statusClassName.Good;
   } else if (avg >= 40 && gas === "SO2") {
-    avg = criteria[norm][gas][3];
     return statusClassName.SuperBad;
   } else if (avg > 0 && avg <= 3999 && gas === "CO") {
-    avg = criteria[norm][gas][0];
     return statusClassName.Good;
   } else if (avg >= 4000 && gas === "CO") {
-    avg = criteria[norm][gas][3];
     return statusClassName.SuperBad;
   }
 }
