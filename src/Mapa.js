643b2e33f9768f767b02d4c8c00c688120206318
Cristian Urbina <urbina1509@gmail.com>
2024-11-25 16:56:11 -0600

    wip

--- src/Mapa.js
+++ src/Mapa.js
@@ -12,6 +12,7 @@ import { Button, Offcanvas } from "react-bootstrap";
 import { BsFillInfoSquareFill } from "react-icons/bs";
 import "./styles/MapLegend.css"
 import { TablaCalidad } from "./components/TablaCalidad.js";
+import useSystemLocations from "./hooks/useSystemLocations.js";
 
 const mapDefaultProps = {
   center: [25.67, -100.25],
@@ -38,10 +39,10 @@ const LegendItem = (props) => {
 };
 
 function MapPage() {
-  const { location, contaminant } = useSelector((state) => state.form);
+  const { location, contaminant, system } = useSelector((state) => state.form);
+  const { locations } = useSystemLocations(system.value);
 
   const [currentInterval, setCurrentInterval] = useState(0);
-  const { sensorData } = useSensorData();
 
   const [map, setMap] = useState(null);
   const [show, setShow] = useState(false);
@@ -62,171 +63,6 @@ function MapPage() {
     setCenter([location?.value.address.lat, location?.value.address.lon]);
   }, [location, setCenter]);
 
-
-
-  //Diccionario para pasar de Sensor_id a nombre de estacion
-  const ANLKeys_dict = {
-    ANL1: "SURESTE",
-    ANL2: "NORESTE",
-    ANL3: "SUROESTE",
-    ANL4: "[SAN Pedro]",
-    ANL5: "NORTE2",
-    ANL6: "GARCIA",
-    ANL7: "NOROESTE",
-    ANL8: "SURESTE3",
-    ANL9: "NORTE",
-    ANL10: "NORESTE2",
-    ANL11: "SUR",
-    ANL12: "CENTRO",
-    ANL13: "SURESTE2",
-    ANL15: "PESQUERIA",
-    ANL16: "MITRAS"
-  };
-
-  const getValue = (preValue, gasName) => {
-    if (!preValue || typeof preValue !== "number" || preValue < 0) {
-      return null;
-    }
-
-    let ans;
-    if (gasName === "NO2" || gasName === "SO2") {
-      ans = +preValue.toFixed(4);
-    } else {
-      ans = +preValue.toFixed(2);
-    }
-    return ans;
-  }
-
-  const filterND = (data) => {
-    return typeof data === "undefined" || data === null || data === ""
-      ? "ND"
-      : data.toString()
-  }
-
-  const markers = useMemo(() => {
-    if (!contaminant) return [];
-    // TODO: sensors should be filtered by the backend
-    const filteredSensors = sensorData?.filter(
-      (sensor) =>
-        typeof sensor.Longitud === "number" &&
-        typeof sensor.Latitud === "number" &&
-        !mapBlacklist.includes(sensor.Sistema) &&
-        !idBlacklist.includes(sensor.Sensor_id)
-    );
-
-
-    const resultingData = filteredSensors?.map((data) => {
-      const { value: gasName } = contaminant;
-      const gasUnits = unidad[gasName];
-
-
-      /* Current Interval === 0 -> Presenta el value del indicador de calidad del aire */
-      let dataKey;
-      if (currentInterval === 0) {
-        dataKey = `ICAR_${gasName}`;
-        if (data.Sistema === "AireNuevoLeon" && gasName === "O3") {
-          if (data[`${dataKey}_1h`] >= data[`${dataKey}_8h`]) {
-            dataKey += "_1h";
-          }
-          else {
-            dataKey += "_8h";
-          }
-        }
-      }
-      else if (currentInterval === 1) {
-        dataKey = gasName;
-        if (gasName === "PM25" && data.Sistema === "PurpleAir") {
-          dataKey += "_Promedio";
-        }
-      }
-      const intValue = getValue(data[dataKey], gasName);
-      const value = toString(intValue) ? intValue : "ND";
-
-      return {
-        currentLocation: location,
-        ICAR_PM25: +data.ICAR_PM25,
-        OMS_PM25: +data.OMS_PM25,
-        AQI_PM25: +data.AQI_PM25,
-        ICAR_PM10: +data.ICAR_PM10,
-        OMS_PM10: +data.OMS_PM10,
-        AQI_PM10: +data.AQI_PM10,
-        ICAR_O3_8h: +data.ICAR_O3_8h,
-        ICAR_O3_1h: +data.ICAR_O3_1h,
-        OMS_O3: +data.OMS_O3,
-        AQI_O3: +data.AQI_O3,
-        ICAR_CO: +data.ICAR_CO,
-        OMS_CO: +data.OMS_CO,
-        AQI_CO: +data.AQI_CO,
-        ICAR_NO2: +data.ICAR_NO2,
-        OMS_NO2: +data.OMS_NO2,
-        AQI_NO2: +data.AQI_NO2,
-        ICAR_SO2: +data.ICAR_SO2,
-        OMS_SO2: +data.OMS_SO2,
-        AQI_SO2: +data.AQI_SO2,
-        sensor_id: data.Sensor_id,
-        humedad: filterND(data.Humedad_R),
-        temperatura: filterND(data.Temperatura_C),
-        position: [data.Latitud, data.Longitud],
-        current: {
-          indicator: contaminant?.label ? contaminant?.label : gasName,
-          label: filterND(value),
-          units: gasUnits,
-          status: getICAR(intValue, gasName, "ssa"),
-          ref: "#",
-        },
-        lastUpdate: moment.utc(data.Dia).local(),
-        locationStr: data.Zona?.length > 0 ? data.Zona : "ND",
-        provider: {
-          name: data.Sistema,
-          ref: (() => {
-            if (data.Sistema === "PurpleAir")
-              return "https://www.purpleair.com/map?mylocation";
-            if (data.Sistema === "AireNuevoLeon")
-              return "http://aire.nl.gob.mx/";
-            if (data.Sistema === "Sinaica")
-              return "https://sinaica.inecc.gob.mx/";
-            return "#";
-          })(),
-        },
-        isPurpleAir: data.Sistema === "PurpleAir",
-        labels: gases.map(({ name, label, units }) => {
-          let colName = name;
-          if (name === "PM25" && data.Sistema === "PurpleAir")
-            colName = "PM25_Promedio";
-          return {
-            label: label ? label : name,
-            units: units,
-            value: filterND(data[colName]),
-            status: getICAR(data[colName], name, "ssa"),
-            ref: "#",
-          };
-        }),
-
-        //url para boton de Mas Informacion
-        urlMI: (() => {
-          if (data.Sistema === "PurpleAir")
-            return (
-              "https://map.purpleair.com/1/mAQI/a10/p0/cC0?select=" +
-              data.Sensor_id.substr(1) +
-              "#13.91/" +
-              data.Latitud +
-              "/" +
-              data.Longitud
-            );
-          if (data.Sistema === "AireNuevoLeon")
-            return (
-              "http://aire.nl.gob.mx:81/SIMA2017reportes/ReporteDiariosimaIcars.php?estacion1=" +
-              ANLKeys_dict[data.Sensor_id]
-            );
-          if (data.Sistema === "Sinaica")
-            return "https://sinaica.inecc.gob.mx/";
-          return "#";
-        })(),
-      };
-    });
-
-    return resultingData;
-  }, [sensorData, contaminant, currentInterval, location]);
 
   return (
     <div>
@@ -278,22 +114,15 @@ function MapPage() {
         </div>
         <Wrapper setMap={setMap} {...mapDefaultProps}>
           {
-            markers.map((markerProps, idx) => {
-              if (!markerProps) return '';
-              if (contaminant?.value === "PM25" || !markerProps?.isPurpleAir) {
-                return (
-                  <Marcador
-                    map={map}
-                    key={idx}
-                    isPurpleAir={markerProps.isPurpleAir}
-                    {...markerProps}
-                    label={markerProps.current.label}
-                    indicator={markerProps.current.indicator}
-                    status={markerProps.current.status}
-                    shape={markerProps.isPurpleAir ? "square" : "round"}
-                  />
-                );
-              }
+            locations.map((loc, idx) => {
+              return (
+                <Marcador
+                  map={map}
+                  key={idx}
+                  sensorId={loc.value.id}
+                  position={[loc.value.address.lat, loc.value.address.lon]}
+                />
+              );
             })}
         </Wrapper>
       </div>
--- src/components/Marcador.js
+++ src/components/Marcador.js
@@ -1,4 +1,5 @@
-import React, { useCallback, useEffect, useMemo, useState } from "react";
+import React, { useCallback, useEffect, useMemo, useRef, useState } from "react";
+import ReactDOMServer from 'react-dom/server';
 import ReactDOM from "react-dom";
 import PropTypes from "prop-types";
 import { divIcon } from "leaflet";
@@ -6,9 +7,10 @@ import "leaflet/dist/leaflet.css";
 import { Marker, Popup } from "react-leaflet";
 import { Button, Container, Col, Row } from "react-bootstrap";
 import moment from "moment";
-import {getStatusClassName} from "../handlers/statusCriteria";
-import {getStatusOMS} from "../handlers/statusCriteria";
-import {getICAR} from "../handlers/statusCriteria";
+import { getStatusClassName } from "../handlers/statusCriteria";
+import { getStatusOMS } from "../handlers/statusCriteria";
+import { getICAR } from "../handlers/statusCriteria";
+import "./Marcador.css";
 
 const fixedValues = {
   PM25: 2,
@@ -35,310 +37,72 @@ const showLabel = (label) => {
   return fixed;
 };
 
-const renderMarker = (label, status, shape = "round", currentLocation, locationStr) => {
-  return (
-    <div
-      className={`${
-        currentLocation === locationStr ? "marker-wrapper-selected" : "marker-wrapper"
-      }`}
-    >
-      <div
-        className={`marker-${status} marker-base marker-shape-${shape} ${
-          currentLocation === locationStr ? "marker-border marker-size-" : ""
-        }`}
-      ></div>
-      <span className={`marker-${status}`} style={{ backgroundColor: "transparent" }}>
-        {showLabel(label)}
-      </span>
-    </div>
-  );
-};
-
 /**
  * Marcador
  * @augments {Component<Props, State>}
  */
 function Marcador({
-  currentLocation,
-  ICAR_PM25,
-  OMS_PM25,
-  AQI_PM25,
-  ICAR_PM10,
-  OMS_PM10,
-  AQI_PM10,
-  ICAR_O3_8h,
-  ICAR_O3_1h,
-  OMS_O3,
-  AQI_O3,
-  ICAR_CO,
-  OMS_CO,
-  AQI_CO,
-  ICAR_NO2,
-  OMS_NO2,
-  AQI_NO2,
-  ICAR_SO2,
-  OMS_SO2,
-  AQI_SO2,
-  sensor_id,
-  humedad,
-  temperatura,
-  label,
-  status,
   position,
-  current,
-  provider,
-  locationStr,
-  labels,
-  urlMI,
-  lastUpdate,
-  map,
-  shape = "round",
-  isPurpleAir,
-  ...props
+  sensorId,
+  status,
 }) {
-  const marker = useMemo(() => document.createElement("div"), []);
-  const [icon, setIcon] = useState(null);
 
-  const values = {
-    ICAR_PM25,
-    OMS_PM25,
-    AQI_PM25,
-    ICAR_PM10,
-    OMS_PM10,
-    AQI_PM10,
-    ICAR_O3_8h,
-    ICAR_O3_1h,
-    OMS_O3,
-    AQI_O3,
-    ICAR_CO,
-    OMS_CO,
-    AQI_CO,
-    ICAR_NO2,
-    OMS_NO2,
-    AQI_NO2,
-    ICAR_SO2,
-    OMS_SO2,
-    AQI_SO2,
-};
-
-
-  const getICARValue = (label) => {
-    const getValue = (label) => {
-      if (label === "O3") {
-        return Math.max(values[`ICAR_O3_8h`], values[`ICAR_O3_1h`]);
-      }
-      return values[`ICAR_${label}`];
-    };
-
-    let ans = getValue(label);
-    if (!isDataValid(ans)) {
-      return "ND";
-    }
-    return ans.toFixed(fixedValues[label]);
-  };
-
-  const getOMSValue = (label) => {
-    let ans = values[`OMS_${label}`];
-    return ans.toFixed(fixedValues);
-  }
-
-  const getAQIValue = (label) => {
-    let ans = values[`AQI_${label}`];
-    return ans.toFixed(fixedValues);
-  }
-
-  const updateMarker = useCallback(
-    (label_, status_, currentLocation_, locationStr_) => {
-      ReactDOM.render(renderMarker(label_, status_, shape, currentLocation_, locationStr_), marker);
-
-      setIcon(
-        divIcon({
-          html: marker,
-          className: `sensor-icon ${
-            currentLocation_ === locationStr_ ? "top" : shape === "round" ? "" : "behind"
-          }`,
-          popupAnchor: [7, 0],
-        })
-      );
-    },
-    [shape, marker]
+  const [icon, setIcon] = useState(
+    divIcon({
+      html: "<div>marker<div>",
+      className: "sensor-icon",
+      popupAnchor: [7, 0],
+    })
   );
-
+  const renderMarker = (label, status, shape = "round") => {
+    return (
+      <div
+        className="marker-wrapper"
+      >
+        <div
+          className={`marker-${status} marker-base marker-shape-${shape}`}
+        ></div>
+        <span className={`marker-${status}`} style={{ backgroundColor: "transparent" }}>
+          {showLabel(label)}
+        </span>
+      </div>
+    );
+  };
+  const renderToHTMLString = (component) => {
+    return ReactDOMServer.renderToString(component);
+  };
+  // const marker = useMemo(() => document.createElement("div"), []);
   useEffect(() => {
-    updateMarker(label, status, currentLocation, locationStr);
-  }, [label, status, updateMarker, currentLocation, locationStr]);
+    setIcon(
+      divIcon({
+        html: renderToHTMLString(renderMarker("lable", "good")),
+        className: "sensor-icon",
+        popupAnchor: [7, 0],
+      })
+    );
+  }, [status]);
 
-  if (!icon) return null;
+  const [data, setData] = useState({});
+  const [opened, setOpened] = useState(false);
+  useEffect(() => {
+    if (!opened) return;
+    console.log("fetching marker data of sensor", sensorId);
+  }, [opened]);
 
   return (
     <Marker
       position={position}
       onClick={(event) => event.target.openPopup()}
       icon={icon}
-      {...props}
+      eventHandlers={{
+        popupopen: () => {
+          setOpened(true);
+        }
+      }}
     >
-      <Popup maxWidth={1700}>
-        <div className="px-3 py-2">
-          <div
-            className={`rounded marker-${current.status}`}
-            style={{
-              display: "flex",
-              justifyContent: "center",
-              padding: "0 0.1rem ",
-            }}
-          >
-            {current.indicator}: {current.label} {current.units}
-          </div>
-
-          <Row className="data-label">
-            <Col xs={5}>
-              <small className="text-muted">Ubicación</small>
-              <br />
-              <data>{locationStr}</data>
-            </Col>
-            <Col xs={5}>
-              <small className="text-muted">Concentración horaria</small>
-              <br />
-              <data><time>{moment(lastUpdate).format("LL, LT")}</time></data>
-            </Col>
-          </Row>
-
-          <Row className="data-label">
-            <Col xs={5}>
-              <small className="text-muted">Humedad</small>
-              <br />
-              <data>{humedad + " %"}</data>
-            </Col>
-            <Col xs={5}>
-              <small className="text-muted">Temperatura</small>
-              <br />
-              <data>{temperatura + " ºC"}</data>
-            </Col>
-          </Row>
-
-          <Container style={{padding: 0, width: "350px"}}>
-            <Row className="flex-nowrap">
-              <Col xs={2} className="px-1 m-1"></Col>
-              <Col xs={3} className="px-1 m-1">
-                <small className="text-muted m-1">Concentración horaria</small>
-              </Col>
-              <Col xs={2} className="px-1 m-1">
-                <small className="text-muted m-1">ICAR*</small>
-              </Col>
-              <Col xs={2} className="px-1 m-1">
-                <small className="text-muted m-1">OMS</small>
-                <br />
-                <p style={{fontSize: "0.57rem"}} className="mb-0">µg/m3</p>
-              </Col>
-              <Col xs={2} className="px-1 m-1">
-                <small className="text-muted m-1">EPA AQI</small>
-              </Col>
-            </Row>
-            {labels.map(({label, status, value, units}, idx) => {
-              if (isPurpleAir && label !="PM2.5") {
-                return;
-              }
-              let cleanLabel = label.replace(".", "");
-              let ICAR_Value = getICARValue(cleanLabel);
-              let OMS_Value = getOMSValue(cleanLabel);
-              let AQI_Value = getAQIValue(cleanLabel);
-              return (<Row className="flex-nowrap">
-                <Col xs={2}>{label}</Col>
-                <Col xs={3} className={`px-1 m-1 rounded d-flex justify-content-between marker-${status}`}>
-                  <div>{value}</div><div> {units}</div>
-                </Col>
-                <Col xs={2} className={`px-1 m-1 rounded marker-${getICAR(ICAR_Value, cleanLabel, "ssa")}`}>{ICAR_Value}</Col>
-                <Col xs={2} className={`px-1 m-1 rounded marker-${getStatusOMS(OMS_Value, cleanLabel, "oms")}`}>{OMS_Value}</Col>
-                <Col xs={2} className={`px-1 m-1 rounded marker-${getStatusClassName(AQI_Value, cleanLabel, "ssa")}`}>{AQI_Value}</Col>
-              </Row>)
-            }
-            )}
-          </Container>
-        <div className="data-label text-center">
-          <small className="text-muted">
-            *Índice de acuerdo a la{" "}
-            <a
-              className="text-black"
-              href="https://www.dof.gob.mx/nota_detalle.php?codigo=5715154&fecha=25/01/2024#gsc.tab=0"
-            >
-              NOM-172-SEMARNAT-2023
-            </a>
-          </small>
-        </div>
-        <div className="py-2 px-3 border-top text-center">
-          <Button size="sm">
-            <a style={{color: "white"}} target="blank" href={urlMI}>
-              Más información
-            </a>
-          </Button>
-          <p className="lh-sm mt-2 mb-0">
-            Fuente(s):{" "}
-            <a target="blank" href={provider.ref}>
-              {provider.name}
-            </a>
-          </p>
-        </div>
-        </div>
-      </Popup>
+      <Popup maxWidth={1700}>hello</Popup>
     </Marker>
   );
 }
-
-Marcador.propTypes = {
-  /**
-   * Etiqueta del indicador que muestra el popup
-   */
-  indicator: PropTypes.string,
-
-  /**
-   * Informacion del indicador que muestra el popup
-   */
-  current: PropTypes.shape({
-    label: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-    status: PropTypes.string,
-    units: PropTypes.string,
-  }),
-
-  /**
-   * Fecha de la ultima actualización del sensor en milisegundos
-   */
-  lastUpdate: PropTypes.instanceOf(moment),
-
-  /**
-   * Etiqueta de la ubicación del sensor
-   */
-  locationStr: PropTypes.string,
-
-  /**
-   * LatLng del marcador
-   */
-  position: PropTypes.arrayOf(PropTypes.number),
-
-  /**
-   * Informacion de la fuente del sensor (e.g. Purple Air, Sinaica, etc.)
-   */
-  provider: PropTypes.shape({
-    name: PropTypes.string,
-    ref: PropTypes.string,
-  }),
-
-  /**
-   * url para boton de Mas Informacion
-   */
-  urlMI: PropTypes.string,
-
-  /**
-   * Informacion de todos los indicadores del sensor
-   */
-  labels: PropTypes.arrayOf(
-    PropTypes.shape({
-      indicator: PropTypes.string,
-      label: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-      status: PropTypes.number,
-      units: PropTypes.string,
-      ref: PropTypes.string,
-    })
-  ),
-};
 
 export default Marcador;
