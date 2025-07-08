11280f288cf850c9b9b66107983d407b34b2a527
Cristian Urbina <urbina1509@gmail.com>
2025-07-07 18:12:10 -0600

    fix env variables not available while building

--- src/constants.js
+++ src/constants.js
@@ -184,8 +184,7 @@ export const idBlacklist = [
 export const idBlacklistpriv = ["P39497", "P39285"];
 export const mapBlacklist = ["Sinaica"];
 
-export const apiUrl = process.env.REACT_APP_API_URL
-console.log(apiUrl)
+export const apiUrl = "https://aire.comiteecologicointegral.org/api"
 
 export const dateFormat = { weekday: "long", month: "short", day: "numeric", year: "numeric" };
 
