e4843d59959d0ec94e1d073d99bc0ba6c3eefef8
Cristian Urbina <urbina1509@gmail.com>
2025-07-19 09:03:39 -0600

    Add sample response of air quality indexes to fill the pop up information

--- src/services/sensorService.js
+++ src/services/sensorService.js
@@ -1,4 +1,5 @@
 
+import mockdata from './sensor_indexes_response_sample.json'
 import { fetchBackendAPI, fetchAPIV2 } from "./api";
 
 export function getSystemSensors (system_name) {
@@ -7,4 +8,15 @@ export function getSystemSensors (system_name) {
 
 export async function getSystemSensorsMetadata(system_name) {
   return fetchAPIV2('/sensors', {system: system_name});
+}
+
+export async function getSensorIndexes(sensor_id) {
+  // TODO: Return real api call response once the endpoint is complete
+  // return fetchAPIV2(`/sensors/{sensor_id}/indexes`);
+  return {
+    ok: true,
+    async json() {
+      return mockdata
+    }
+  }
 }
